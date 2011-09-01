db88e3e3594f58d3ae99562f7a22da318dc71842
Gabor Cselle <gabor@google.com>
2011-09-01 13:41:15 -0700

    Windows port for LevelDB based on Boost libraries. See files README and WINDOWS for details.

--- util/posix_logger.h
+++ util/posix_logger.h
@@ -1,8 +1,9 @@
-// Copyright 2011 Google Inc. All Rights Reserved.
-// Author: sanjay@google.com (Sanjay Ghemawat)
-//
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
 // Logger implementation that can be shared by all environments
-// where enough posix functionality is available.
+// where enough Posix functionality is available.
 
 #ifndef STORAGE_LEVELDB_UTIL_POSIX_LOGGER_H_
 #define STORAGE_LEVELDB_UTIL_POSIX_LOGGER_H_
