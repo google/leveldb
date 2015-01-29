dd598676cd655dc2a2aaef47715ce18175d4a550
Wankai Zhang <wankaizhang@gmail.com>
2015-01-29 14:15:31 +0800

    block_builder header file dependency fixed

--- table/block_builder.cc
+++ table/block_builder.cc
@@ -30,8 +30,8 @@
 
 #include <algorithm>
 #include <assert.h>
+#include "leveldb/options.h"
 #include "leveldb/comparator.h"
-#include "leveldb/table_builder.h"
 #include "util/coding.h"
 
 namespace leveldb {
