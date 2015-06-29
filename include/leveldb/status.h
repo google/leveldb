5208e7952d691e256a4ffe29888154068e8745b8
cmumford <cmumford@google.com>
2015-06-29 14:40:59 -0700

    Added leveldb::Status::IsInvalidArgument() method.
    All other Status::Code enum values have an Is**() method with the one
    exception of InvalidArgument.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=97166441

--- include/leveldb/status.h
+++ include/leveldb/status.h
@@ -63,6 +63,9 @@ class Status {
   // Returns true iff the status indicates a NotSupportedError.
   bool IsNotSupportedError() const { return code() == kNotSupported; }
 
+  // Returns true iff the status indicates an InvalidArgument.
+  bool IsInvalidArgument() const { return code() == kInvalidArgument; }
+
   // Return a string representation of this status suitable for printing.
   // Returns the string "OK" for success.
   std::string ToString() const;
