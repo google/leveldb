22d5910e8e0a9cea8cec3d327b4f2afe94aad254
Yuki Nagai <unynagai@gmail.com>
2018-03-22 15:45:13 +0900

    Carthage Support.

---
+++ xcode/leveldb/leveldb.h
@@ -0,0 +1,24 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for leveldb.
+FOUNDATION_EXPORT double leveldbVersionNumber;
+
+//! Project version string for leveldb.
+FOUNDATION_EXPORT const unsigned char leveldbVersionString[];
+
+#import <leveldb/c.h>
+#import <leveldb/cache.h>
+#import <leveldb/comparator.h>
+#import <leveldb/db.h>
+#import <leveldb/dumpfile.h>
+#import <leveldb/env.h>
+#import <leveldb/export.h>
+#import <leveldb/filter_policy.h>
+#import <leveldb/iterator.h>
+#import <leveldb/leveldb.h>
+#import <leveldb/options.h>
+#import <leveldb/slice.h>
+#import <leveldb/status.h>
+#import <leveldb/table_builder.h>
+#import <leveldb/table.h>
+#import <leveldb/write_batch.h>
