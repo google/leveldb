c43565dd398b2233db8eb49ba05234d62fb42e03
costan <costan@google.com>
2018-09-17 23:05:57 -0700

    C++11 cleanup for util/mutexlock.h.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=213398583

--- util/mutexlock.h
+++ util/mutexlock.h
@@ -28,11 +28,11 @@ class SCOPED_LOCKABLE MutexLock {
   }
   ~MutexLock() UNLOCK_FUNCTION() { this->mu_->Unlock(); }
 
+  MutexLock(const MutexLock&) = delete;
+  MutexLock& operator=(const MutexLock&) = delete;
+
  private:
   port::Mutex *const mu_;
-  // No copying allowed
-  MutexLock(const MutexLock&);
-  void operator=(const MutexLock&);
 };
 
 }  // namespace leveldb
