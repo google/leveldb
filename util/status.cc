ae49533210e96bdee9c9479a7fa547f375a39c8b
Chris Mumford <cmumford@google.com>
2019-05-22 15:19:38 -0700

    Add explicit typecasts to avoid compiler warning.
    Fixes issue #684.
    
    PiperOrigin-RevId: 249531001

--- util/status.cc
+++ util/status.cc
@@ -20,8 +20,8 @@ const char* Status::CopyState(const char* state) {
 
 Status::Status(Code code, const Slice& msg, const Slice& msg2) {
   assert(code != kOk);
-  const uint32_t len1 = msg.size();
-  const uint32_t len2 = msg2.size();
+  const uint32_t len1 = static_cast<uint32_t>(msg.size());
+  const uint32_t len2 = static_cast<uint32_t>(msg2.size());
   const uint32_t size = len1 + (len2 ? (2 + len2) : 0);
   char* result = new char[size + 5];
   memcpy(result, &size, sizeof(size));
