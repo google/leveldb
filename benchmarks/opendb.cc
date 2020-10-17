b2515c526a244086627fab2f89f1f0913481eba6
omkardesai <omkar.desai@practo.com>
2020-10-17 10:53:38 -0400

    removing memtable key seperation testing

--- benchmarks/opendb.cc
+++ benchmarks/opendb.cc
@@ -8,19 +8,27 @@ int main(int argc, char* argv[]) {
   leveldb::Options options;
   options.create_if_missing = true;
   leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db);
-  std::cout << status.ToString()<<"\n";
   std::string value, key, return_value;
   leveldb::Status s;
-  for(int i = 0; i<500000;i++) {
+  for(int i = 0; i<50;i++) {
     value = "value" + std::to_string(i);
-    key = "key" + std::to_string(i);
+    key = std::to_string(i);
     s = db->Put(leveldb::WriteOptions(), key, value);
     //std::cout << return_value << "\n";
   }
   leveldb::Iterator* db_iter = db->NewIterator(leveldb::ReadOptions());
+  s=db->Get(leveldb::ReadOptions(), "4", &value);
+  std::cout << value;
   db_iter->SeekToFirst();
+  while(db_iter->Valid()){
+    leveldb::Slice value_ = db_iter->value();
+    leveldb::Slice key_ = db_iter->key();
+    //std::cout << key_.ToString()<<" <--> " << value_.ToString() << "\n";
+    db_iter->Next();
+  }
   //leveldb::Slice slice = db_iter->value();
   //std::cout << slice.ToString() << "\n";
+  /*
   std::string property;
   db->GetProperty("leveldb.stats", &property);
   std::cout << property << "\n";
@@ -30,6 +38,7 @@ int main(int argc, char* argv[]) {
   std::cout << property << "\n";
   db->GetProperty("leveldb.sstables", &property);
   std::cout << property << "\n";
+   */
   assert(status.ok());
   delete db;
 
