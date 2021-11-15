d495cbb09588c3b42173abefb6e31d12791c6766
baiqiubai <2569722395@qq.com>
2021-11-15 14:59:33 +0800

    fixtestdocument

--- db/db_test.cc
+++ db/db_test.cc
@@ -694,7 +694,7 @@ TEST_F(DBTest, IterateOverEmptySnapshot) {
   } while (ChangeOptions());
 }
 
-TEST_F(DBTest, GetLevel0Ordering) {
+TEST_F(DBTest, GetLevel2Ordering) {
   do {
     // Check that we process level-2 files in correct order.  The code
     // below generates two level-2 files where the earlier one comes
